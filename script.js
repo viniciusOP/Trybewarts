755e2c92522a535fc09daf25fbb554ba46180624
viniciusOP <viniciusopassos@hotmail.com>
2022-10-03 17:55:31 -0300

    feat: criar card com o conteudo do formulario

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const getEmail = document.getElementById('input-email');
+const getEmail = document.getElementById('email');
 const getPassword = document.getElementById('input-password');
 const getButtonLogin = document.getElementById('login-button');
 const getButtonSubmit = document.getElementById('submit-btn');
 const checkBoxAgreement = document.querySelector('#agreement');
 const getCounter = document.querySelector('#counter');
 const getTextArea = document.querySelector('#textarea');
-const getTextAreaValue = document.querySelector('#textarea').value;
+const form = document.getElementById('evaluation-form');
+const firstName = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const formEmail = document.getElementById('input-email');
+const house = document.getElementById('house');
 
 getButtonSubmit.disabled = true;
 
@@ -26,10 +30,32 @@ function disabledSubmit() {
 }
 
 function characterCounter() {
+  const getTextAreaValue = document.querySelector('#textarea').value;
   const character = (500 - getTextAreaValue.length);
   getCounter.innerHTML = character;
+}
+
+function formCard(e) {
+  e.preventDefault();
+  const avaliation = document.querySelector('input[name="rate"]:checked');
+  const family = document.querySelector('input[name="family"]:checked');
+  const tools = document.getElementsByName('tools');
+  const toolsChecked = [];
+  for (let i = 0; i < tools.length; i += 1) {
+    if (tools[i].checked === true) {
+      toolsChecked.push(tools[i].value);
+    }
+  }
+  form.innerHTML = `<p>Nome: ${firstName.value} ${lastName.value}</p>
+    <p>Email: ${formEmail.value}</p>
+    <p>Casa: ${house.value}</p>
+    <p>Família: ${family.value}</p>
+    <p>Matérias: ${toolsChecked}</p>
+    <p>Avaliação: ${avaliation.value}</p>
+    <p>Observações: ${getTextArea.value}</p>`;
 }
 
 getButtonLogin.addEventListener('click', loginValidation);
 checkBoxAgreement.addEventListener('change', disabledSubmit);
 getTextArea.addEventListener('input', characterCounter);
+getButtonSubmit.addEventListener('click', formCard);
